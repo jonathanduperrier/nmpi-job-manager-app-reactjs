c688672ea14e467bc17f0822a9451a84fb92080e
Jonathan Duperrier <jonathan.duperrier@gmail.com>
2020-02-20 17:29:32 +0100

    .

--- src/Queue/DetailQueue.js
+++ src/Queue/DetailQueue.js
@@ -1,12 +1,21 @@
-import React, {Component, useImperativeHandle} from 'react';  
+import React, {Component, useImperativeHandle} from 'react';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function DetailQueue() {
+  let { id } = useParams(); //impossible with class component
 
-    return (
-      <div>
-        <h2>Detail Queue</h2>
-      </div>
-    );
+  return(
+    <div>
+      <h2>Detail Queue {id}</h2>
+    </div>
+  );
 }
 
 export default DetailQueue;
