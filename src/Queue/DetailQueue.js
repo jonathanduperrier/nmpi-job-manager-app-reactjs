17f0b0d53a6f2b5d2a586576829d30880dcb37fc
Jonathan Duperrier <jonathan.duperrier@gmail.com>
2020-02-27 16:49:01 +0100

    .

--- src/Queue/DetailQueue.js
+++ src/Queue/DetailQueue.js
@@ -10,19 +10,21 @@ import {
 
 function DetailQueue() {
   let { id } = useParams(); //impossible with class component
+
   axios.get('https://raw.githubusercontent.com/jonathanduperrier/nmpi-job-manager-app-reactjs/master/db_' + id + '.json')
-    .then(response => {
-      console.log(response);
-      // this.setState({jobs: response.data.objects});
-      // var mydate = new Date(response.data.objects.date);
-      // var date = mydate.toString("jj/MM/yyyy");
-      // console.log("date : " + date);
-      // this.setState({date: date});
-    });
+  .then(response => {
+    console.log(response.data.objects[0]);
+    //this.setState({job: response.data.objects[0]});
+    // var mydate = new Date(response.data.objects.date);
+    // var date = mydate.toString("jj/MM/yyyy");
+    // console.log("date : " + date);
+    // this.setState({date: date});
+  });
 
   return(
     <div>
-      <h2>Detail Queue {id}</h2>
+      <h2>Job {id}</h2>
+
     </div>
   );
 }
--- src/Queue/ListQueue.js
+++ src/Queue/ListQueue.js
@@ -13,7 +13,6 @@ class ListQueue extends Component {
 
   constructor(props) {
     super(props)
-  
     this.state = {
       jobs: [],
       error: '',
