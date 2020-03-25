0a3ab946e5de68c011b96270107a8230644558fe
jonathanduperrier <jonathan.duperrier@gmail.com>
2020-03-25 22:16:59 +0100

    solution founded to fetch and display data provided by axios request in functionnal components

--- src/Queue/DetailQueue.js
+++ src/Queue/DetailQueue.js
@@ -1,4 +1,4 @@
-import React, {Component, useImperativeHandle} from 'react';
+import React, {Component, useImperativeHandle, useState, useEffect} from 'react';
 import axios from 'axios';
 import {
   BrowserRouter as Router,
@@ -10,19 +10,42 @@ import {
 
 function DetailQueue() {
   let { id } = useParams(); //impossible with class component
-  axios.get('https://raw.githubusercontent.com/jonathanduperrier/nmpi-job-manager-app-reactjs/master/db_' + id + '.json')
-    .then(response => {
-      console.log(response);
-      // this.setState({jobs: response.data.objects});
-      // var mydate = new Date(response.data.objects.date);
-      // var date = mydate.toString("jj/MM/yyyy");
-      // console.log("date : " + date);
-      // this.setState({date: date});
-    });
+  const[jobs, setJobs] = useState({objects : []});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://raw.githubusercontent.com/jonathanduperrier/nmpi-job-manager-app-reactjs/master/db_' + id + '.json',
+      );
+      setJobs(result.data);
+    };
+    fetchData();
+  }, []);
 
   return(
     <div>
-      <h2>Detail Queue {id}</h2>
+      <h2>Job {id}</h2>
+        {jobs.objects.map(job => (
+          <div>
+            <p>
+              <span className={job.status == 'finished' ? 'badge badge-success' : 'badge badge-danger'}>{job.status}</span>
+            </p>
+            <p>
+              <small class="ng-binding">Submitted {job.timestamp_submission}
+                    by DUPERRIER Jonathan
+                    to <strong class="ng-binding">{job.hardware_platform}</strong>
+              </small>
+            </p>
+            <div class="row-fluid">
+              <div class="panel panel-default">
+                <div class="panel-heading">Code</div>
+                <div class="panel-body ng-pristine ng-untouched ng-binding ng-scope ng-valid ng-valid-required">
+                  {job.code}
+                </div>
+              </div>
+            </div>
+          </div>
+      ))}
     </div>
   );
 }
